98bc39ec303b8a1bd0a515f7f35353237c61971c
waldo237 <waldo23792@gmail.com>
2021-04-02 21:12:29 -0400

    added some utilities

--- src/functions/util.ts
+++ src/functions/util.ts
@@ -1,21 +1,28 @@
-function pickOne(selection:[]){
-  const  index=  Math.round((Math.random() * selection.length-1)+0);
-return selection[index];
+function pickOne(selection: []) {
+    const index = Math.round((Math.random() * selection.length - 1) + 0);
+    return selection[index];
 }
-
+// TODO turn to recursion
 export function shuffle<T>(array: T[]) {
     for (let i = array.length - 1; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * (i + 1));
-      swap(array, i, randomIndex);
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        swap(array, i, randomIndex);
     }
-  
     return array;
-  }
-  
-  export function swap(array: any[], a: number, b: number) {
-    /* const temp = array[a];
-    array[a] = array[b];
-    array[b] = temp; */
-    [array[a], array[b]] = [array[b], array[a]];
-  }
-  
+}
+
+function shuffleWithRecursion<T>(items: T[]) {
+    const index = items.length - 1;
+    return shuffler(items, index);
+}
+
+function shuffler<T>(items: T[], itemsLeft: number): T[] {
+    if (itemsLeft === 0) { return items; } //base case
+
+    const randomNum: number = Math.floor(Math.random() * itemsLeft--);
+    return shuffler(swap(items, itemsLeft, randomNum), itemsLeft--);
+}
+
+export function swap<T>(array: T[], a: number, b: number): T[] {
+    return [array[a], array[b]] = [array[b], array[a]];
+}
