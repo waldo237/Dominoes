3e1db45afbf9751c7b478ec20c69b11371849939
waldo237 <waldo23792@gmail.com>
2021-04-05 14:28:19 -0400

    added console interaction, continued refactoring the classes.

--- src/models/DominoesChain.ts
+++ src/models/DominoesChain.ts
@@ -1,22 +1,17 @@
 import { Domino } from "./Domino";
 import { Leads } from "./Leads";
 
-enum itGoes {
-    TO_THE_HEAD,
-    TO_THE_TAIL,
-    NOWHERE
-}
 
 /**
  * A data structure that represents the chain of 
- * Dominoes on a table. Its store is an array, not a 
+ * Dominoes on a table. Its _store is an array, not a 
  * DoublyLinkedList because it facilitates the interaction
  * with the other objects that use arrays.
  * 
  */
 class DominoesChain {
     public static instance: DominoesChain
-    private store: Domino[] = [];
+    private _store: Domino[] = [];
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     private constructor() { }
@@ -26,132 +21,140 @@ class DominoesChain {
         }
         return DominoesChain.instance;
     }
+    public get store(): Domino[] {
+        return this._store;
+    }
+
     private insertAtHead(domino: Domino): void {
-        this.store = [domino, ...this.store];
+        this._store = [domino, ...this._store];
     }
     private insertAtTail(domino: Domino): void {
-        this.store.push(domino);
+        this._store.push(domino);
     }
     public reset(): void {
-        this.store = [];
+        this._store = [];
     }
     public isEmpty(): boolean {
-        return this.store.length === 0;
+        return this._store.length === 0;
     }
     private getHead(): Domino {
-        return this.store[0];
+        return this._store[0];
     }
     private getTail(): Domino {
-        return this.store[this.store.length - 1];
+        return this._store[this._store.length - 1];
     }
     private getSize(): number {
-        return this.store.length;
-    }
-
-    /**
-     * helper function to abstract logic
-     * @param dominoSide 
-     * @returns itGoes enum
-     */
-    placeOf(dominoSide: number): itGoes {
-        const head = this.getHead();
-        const tail = this.getTail();
-        if (head.firstInserted && (dominoSide === head.side1 || dominoSide === head.side2)) {
-            return itGoes.TO_THE_TAIL;
-        }
-        else if (dominoSide === head.next) {
-            return itGoes.TO_THE_HEAD
-        } else if (dominoSide === tail.next) {
-            return itGoes.TO_THE_TAIL;
-        }
-        return itGoes.NOWHERE;
-    }
-
-    /**
-     * helper function to determine the lead either of the head or tail
-     * It sets the 'next' and the 'direction' fields, does the insert and returns a boolean
-     * @param selectedSide 
-     * @param theOtherSide 
-     * @param domino 
-     * @returns boolean
-     */
-    determineLead(selectedSide: number, theOtherSide: number, domino: Domino): boolean {
-        let result = false;
-        switch (this.placeOf(selectedSide)) {
-            case itGoes.TO_THE_HEAD:
-                domino.next = theOtherSide;
-                domino.direction = this.getHead().direction;
-                this.insertAtHead(domino);
-                result = true;
-                break;
-            case itGoes.TO_THE_TAIL:
-                domino.next = theOtherSide;
-                domino.direction = this.getTail().direction;
-                this.insertAtHead(domino);
-                result = true;
-                break;
-            case itGoes.NOWHERE:
-                result = false;
-                break;
-        }
-        return result;
+        return this._store.length;
     }
 
     /**
      * Follows a complex control flow to simulate laying a domino on a table.
-     * @param domino 
+     * @param domino 🧨🧨🧨🧨🧨🧨🧨🧨🧨NEEDS TO BE REFACTORED BECAUSE IT'S TOO REPETIIVE🎇🎇🧨🧨🧨🎇🧨
      * @returns void
      */
     public addDomino(domino: Domino): void {
         const { side1, side2 } = domino;
-
-        if (this.isEmpty()) { //if the store is empty
+        if (this.isEmpty()) { //if the _store is empty
             if (domino.isDouble()) {
                 // if both faces of the domino are equal, any of them can be the next.
-                domino.firstInserted;
+                domino.firstInserted = true;
                 domino.direction = false;
-                this.insertAtHead(domino);
+                this.insertAtTail(domino);
             } else {
                 // the next dominoes don't need to know the direction, its only one.
                 domino.direction = false;
                 //Signal that any of the sides can be next.
-                domino.firstInserted;
-                this.insertAtHead(domino);
+                domino.firstInserted = true;
+                this.insertAtTail(domino);
             }
         } else {
             // The second domino in the chain. if so set the direction to true.
-            if (this.store.length === 1) {
-                const firstDomino = this.store[0];
+            if (this._store.length === 1) {
+                const firstDomino = this._store[0];
                 // check against the first domino
-                if (side1 === firstDomino.side1 || side1 === firstDomino.side2) {
+                if (side1 === firstDomino.side1) {
                     domino.next = side2;
-                } else if (side2 === firstDomino.side1 || side2 === firstDomino.side2) {
+                    domino.direction = true; //initialize a direction
+                    return this.insertAtTail(domino);
+                } else if (side1 === firstDomino.side2) {
+                    domino.next = side2;
+                    domino.direction = true; //initialize a direction
+                    return this.insertAtTail(domino);
+                } else if (side2 === firstDomino.side1) {
                     domino.next = side1;
+                    domino.direction = true; //initialize a direction
+                    return this.insertAtTail(domino);
+                }else if (side2 === firstDomino.side2) {
+                    domino.next = side1;
+                    domino.direction = true; //initialize a direction
+                    return this.insertAtTail(domino);
                 }
-                domino.direction = true; //initialize a direction
-                return this.insertAtTail(domino);
             }
-            //DRY:call a switch backwards if first doesn't pass
-            const passed = this.determineLead(side1, side2, domino);
-            if (!passed) this.determineLead(side2, side1, domino);
+            const head = this.getHead();
+            const tail = this.getTail();
+           
+            if ( side1 === head.side1 && head.firstInserted){
+                domino.next = side2;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            }else if( side1 === head.side2 && head.firstInserted){
+                domino.next = side2;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            }else if ( side2 === head.side1 && head.firstInserted){
+                domino.next = side2;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            }else if( side2 === head.side2 && head.firstInserted){
+                domino.next = side2;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            }
+              
+            if (side1 === head.next) {
+                domino.next = side2;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            } else if (side1 === tail.next) {
+                domino.next = side2;
+                domino.direction = tail.direction;
+                this.insertAtTail(domino);
+            }
+            else if (side2 === head.next) {
+                domino.next = side1;
+                domino.direction = head.direction;
+                this.insertAtHead(domino);
+            } else if (side2 === tail.next) {
+                domino.next = side1;
+                domino.direction = tail.direction;
+                this.insertAtTail(domino);
+            }
         }
     }
 
     /**
-     * shows the outside whats on the two ends of the store
+     * shows the outside whats on the two ends of the _store
      */
-    public showLeads(): Leads {
+    public showLeads(): Leads |null {
         // if there is only one domino and is double, both of its sides will be the two leads.
+        const head = this.getHead();
+        const tail = this.getTail();
+        if(!head){
+            return null; 
+        }
         if (this.getSize() === 1) {
-            const lead1 = this.getHead().side1;
-            const lead2 = this.getTail().side2;
+            const lead1 = head.side1;
+            const lead2 = tail.side2;
+            const leads = new Leads(lead1, lead2);
+            return leads;
+        }else{
+            const lead1 = tail.next;
+            const lead2 = tail.next;
+
             const leads = new Leads(lead1, lead2);
             return leads;
         }
-        const lead1 = this.getHead().next;
-        const lead2 = this.getTail().next;
-        const leads = new Leads(lead1, lead2);
-        return leads;
     }
+
 }
 export default DominoesChain;
